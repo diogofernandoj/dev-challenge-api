b1c32bb2238c73a2afac6b4f3d455eb4ff8b3b85
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-04 14:39:20 -0300

    feat: add delete sport route to index file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import { CreateSportController } from './src/controllers/sport/create-sport.js'
+import { DeleteSportController } from './src/controllers/delete-sport.js'
 
 const app = express()
 
@@ -9,6 +10,13 @@ app.use(express.json())
 app.post('/', async (req, res) => {
     const createSportController = new CreateSportController()
     const { statusCode, body } = await createSportController.execute(req)
+
+    res.status(statusCode).send(body)
+})
+
+app.delete('/:sport_id', async (req, res) => {
+    const deleteSportController = new DeleteSportController()
+    const { statusCode, body } = await deleteSportController.execute(req)
 
     res.status(statusCode).send(body)
 })
