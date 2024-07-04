92f3181ea50b04160aa08b6a8b224d35891e2551
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-04 14:22:46 -0300

    feat: ensure strict params can be passed only

--- src/schemas/sport.js
+++ src/schemas/sport.js
@@ -1,25 +1,29 @@
 import { z } from 'zod'
 
-export const createSportSchema = z.object({
-    name: z
-        .string({
-            required_error: 'Name is required',
-        })
-        .trim()
-        .min(1, {
-            message: 'Name is required',
+export const createSportSchema = z
+    .object({
+        name: z
+            .string({
+                required_error: 'Name is required',
+            })
+            .trim()
+            .min(1, {
+                message: 'Name is required',
+            }),
+        description: z
+            .string({
+                required_error: 'Description is required',
+            })
+            .trim()
+            .min(1, {
+                message: 'Description is required',
+            }),
+        difficulty_level: z.enum(['EASY', 'MEDIUM', 'HARD'], {
+            errorMap: () => ({
+                message: 'Difficulty level must be EASY, MEDIUM or HARD.',
+            }),
         }),
-    description: z
-        .string({
-            required_error: 'Description is required',
-        })
-        .trim()
-        .min(1, {
-            message: 'Description is required',
-        }),
-    difficulty_level: z.enum(['EASY', 'MEDIUM', 'HARD'], {
-        errorMap: () => ({
-            message: 'Difficulty level must be EASY, MEDIUM or HARD.',
-        }),
-    }),
-})
+    })
+    .strict({
+        message: 'Some provided field is not allowed',
+    })
