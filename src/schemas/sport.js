fb669773b2d4c3730626ec2065d76f540995804d
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-03 23:43:46 -0300

    feat: add sport schema with zod

---
+++ src/schemas/sport.js
@@ -0,0 +1,25 @@
+import { z } from 'zod'
+
+export const createSportSchema = z.object({
+    name: z
+        .string({
+            required_error: 'Name is required',
+        })
+        .trim()
+        .min(1, {
+            message: 'Name is required',
+        }),
+    description: z
+        .string({
+            required_error: 'Description is required',
+        })
+        .trim()
+        .min(1, {
+            message: 'Description is required',
+        }),
+    difficulty_level: z.enum(['EASY', 'MEDIUM', 'HARD'], {
+        errorMap: () => ({
+            message: 'Difficulty level must be EASY, MEDIUM or HARD.',
+        }),
+    }),
+})
