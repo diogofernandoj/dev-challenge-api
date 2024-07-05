6998cf69445a507b5ed73030e038417ab6c1e0d6
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-04 21:30:54 -0300

    feat: add GetSportById route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import { CreateSportController } from './src/controllers/sport/create-sport.js'
 import { DeleteSportController } from './src/controllers/sport/delete-sport.js'
 import { GetSportsController } from './src/controllers/sport/get-sports.js'
+import { GetSportByIdController } from './src/controllers/sport/get-sport-by-id.js'
 
 const app = express()
 
@@ -11,6 +12,13 @@ app.use(express.json())
 app.get('/sports', async (_req, res) => {
     const getSportsController = new GetSportsController()
     const { statusCode, body } = await getSportsController.execute()
+
+    res.status(statusCode).send(body)
+})
+
+app.get('/sports/:sport_id', async (req, res) => {
+    const getSportByIdController = new GetSportByIdController()
+    const { statusCode, body } = await getSportByIdController.execute(req)
 
     res.status(statusCode).send(body)
 })
