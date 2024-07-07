dcffbde18991a7e0811ca755190b17c58e404280
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-07 10:55:42 -0300

    test: ensure UpdateSportController returns 200 on updating a sport successfully

---
+++ src/controllers/sport/update-sport.test.js
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker'
+
+import { UpdateSportController } from './update-sport.js'
+import { DifficultyLevel } from '@prisma/client'
+
+describe('UpdateSportsController', () => {
+    class UpdateSportsRepositoryStub {
+        async execute() {
+            return true
+        }
+    }
+
+    const makeSut = () => {
+        const updateSportsRepository = new UpdateSportsRepositoryStub()
+        const sut = new UpdateSportController(updateSportsRepository)
+
+        return { sut, updateSportsRepository }
+    }
+
+    const httpRequest = {
+        params: {
+            sport_id: faker.string.uuid(),
+        },
+        body: {
+            name: faker.string.alpha(10),
+            description: faker.string.alpha(20),
+            difficulty_level: faker.helpers.enumValue(DifficultyLevel),
+        },
+    }
+
+    it('should return 200 on updating a sport successfully', async () => {
+        const { sut } = makeSut()
+
+        const res = await sut.execute(httpRequest)
+
+        expect(res.statusCode).toBe(200)
+    })
+})
